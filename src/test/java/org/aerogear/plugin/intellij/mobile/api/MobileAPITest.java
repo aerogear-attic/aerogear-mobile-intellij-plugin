832e63b0298d3dc04d746a464574b4aef1d37afb
Jameel Briones <jbriones@redhat.com>
2018-01-26 14:56:47 +0000

    setup testng with gradle

---
+++ src/test/java/org/aerogear/plugin/intellij/mobile/api/MobileAPITest.java
@@ -0,0 +1,13 @@
+package org.aerogear.plugin.intellij.mobile.api;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MobileAPITest {
+    @Test
+    public void testGetServices() {
+        System.out.println("Get Services Test");
+        Assert.assertEquals(0, 0);
+    }
+
+}
