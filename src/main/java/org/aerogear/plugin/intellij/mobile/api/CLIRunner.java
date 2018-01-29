89e5157706658de926d4cc9444eea5d80f73d7ea
witmicko <mogrodni@redhat.com>
2018-01-29 12:32:27 +0000

    initial

--- src/main/java/org/aerogear/plugin/intellij/mobile/api/CLIRunner.java
+++ src/main/java/org/aerogear/plugin/intellij/mobile/api/CLIRunner.java
@@ -3,6 +3,7 @@ package org.aerogear.plugin.intellij.mobile.api;
 import java.util.List;
 
 public interface CLIRunner {
-  String executeSync(List<String> command)throws CLIException;
-  void executeAsync(List<String> command, Watcher w);
+    String executeSync(List<String> command) throws CLIException;
+
+    void executeAsync(List<String> command, Watcher w);
 }
