dd6696126ee31e74cd4a0fb905b23dc29f107c87
dimitraz <dzuccare@redhat.com>
2018-02-16 14:55:39 +0000

    Check for existing client

--- src/main/java/org/aerogear/plugin/intellij/mobile/ui/actions/ClientCreatedCheck.java
+++ src/main/java/org/aerogear/plugin/intellij/mobile/ui/actions/ClientCreatedCheck.java
@@ -1,5 +1,6 @@
 package org.aerogear.plugin.intellij.mobile.ui.actions;
 
+import com.google.gson.Gson;
 import com.intellij.notification.NotificationListener;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
@@ -7,6 +8,8 @@ import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.aerogear.plugin.intellij.mobile.api.CLIException;
+import org.aerogear.plugin.intellij.mobile.api.CLIRunner;
 import org.aerogear.plugin.intellij.mobile.api.CLIRunnerImpl;
 import org.aerogear.plugin.intellij.mobile.api.MobileAPI;
 import org.aerogear.plugin.intellij.mobile.services.MobileNotificationsService;
@@ -14,7 +17,13 @@ import org.aerogear.plugin.intellij.mobile.services.configuration.ProjectConfigu
 import org.aerogear.plugin.intellij.mobile.ui.MobileIcons;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class ClientCreatedCheck implements StartupActivity {
+
+    private CLIRunner cliRunner = CLIRunnerImpl.getInstance();
 
     @Override
     public void runActivity(@NotNull Project project) {
@@ -23,25 +32,67 @@ public class ClientCreatedCheck implements StartupActivity {
     }
 
     private void checkFile(String filePath, @NotNull Project project) {
+
+        MobileAPI mobileAPI = new MobileAPI(cliRunner);
         VirtualFile file = ApplicationManager.getApplication().runReadAction((Computable<VirtualFile>) () -> {
             return LocalFileSystem.getInstance().refreshAndFindFileByPath(filePath);
         });
 
+        // If a client representation does not exist, prompt the user to create one
         if (file == null) {
-            MobileAPI mobileAPI = new MobileAPI(CLIRunnerImpl.getInstance());
-
-            NotificationListener notificationListener = (notification, event) -> {
-                String appId = ProjectConfiguration.getInstance(project).getAppId(project);
-                new ClientCreatedCheckAction().showCreateClientForm(project, mobileAPI, filePath, appId);
-            };
-
-            new MobileNotificationsService().notifyWarning(
-                    MobileIcons.AEROGEAR,
-                    "<html>Client app definition not found",
-                    " <a href=\"" + "here" + "\" target=\"blank\">Create new client app definition</a> </html>",
-                    notificationListener
-            );
+            String title = "Client app definition not found";
+            createClientNotification(project, mobileAPI, filePath, title);
         }
+        // If there is a client representation, check to see that the client still exists
+        else {
+            Gson gson = new Gson();
+
+            try {
+                // Get the client config as properties
+                String configFile = new String(file.contentsToByteArray());
+                Properties clientConfig = gson.fromJson(configFile, Properties.class);
+                String clientId = clientConfig.getProperty("name") + '-' + clientConfig.getProperty("clientType");
+
+                if (!clientExists(clientId)) {
+                    String title = "Client " + clientId + " no longer exists in current namespace";
+                    createClientNotification(project, mobileAPI, filePath, title);
+                }
+            } catch (IOException e) {
+                throw new CLIException("Unable to read client configuration");
+            }
+        }
+    }
+
+    /**
+     * Reusable balloon notification which prompts a user to create
+     * a new client representation
+     */
+    private void createClientNotification(@NotNull Project project, MobileAPI mobileAPI, String filePath, String title) {
+        NotificationListener notificationListener = (notification, event) -> {
+            String appId = ProjectConfiguration.getInstance(project).getAppId(project);
+            new ClientCreatedCheckAction().showCreateClientForm(project, mobileAPI, filePath, appId);
+        };
+
+        new MobileNotificationsService().notifyWarning(
+                MobileIcons.AEROGEAR,
+                "<html>" + title,
+                " <a href=\"" + "here" + "\" target=\"blank\">Create a new client app definition</a> </html>",
+                notificationListener
+        );
+    }
+
+    /**
+     * Utility method to check whether the client exists on the connected
+     * cluster
+     */
+    private Boolean clientExists(String clientId) {
+        try {
+            cliRunner.executeSync(Arrays.asList("get", "client", clientId, "--", "-o=json"));
+        } catch (CLIException e) {
+            return false;
+        }
+
+        return true;
     }
 
 }
